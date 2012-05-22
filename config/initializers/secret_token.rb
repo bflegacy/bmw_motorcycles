284042a962824586dd18864b13280d050c9cc807
Jason Lynes <jason.lynes@gmail.com>
2012-05-22 17:00:41 -0400

    f

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmwmoto::Application.config.secret_token = '70f74a3ae83728a37b3123270cabe2b971ecf5cbf539c9d63b7e6ce453934836a8840756424ff8c2708633e85e1df556a41c8b911ea2ecbed65304b7f6fbc7d0'
